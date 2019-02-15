22bb49100cf9086aa561fba2554317dc1902b2e1
Nikita Mamro <43335327+zzzloba@users.noreply.github.com>
2019-02-15 23:14:51 +0300

    Added trivial tests

--- Lab1/Task1/Homework/Program.cs
+++ Lab1/Task1/Homework/Program.cs
@@ -6,6 +6,16 @@ namespace Homework
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Запуск тестов...");
+
+            if (!Test.TestFactorial())
+            {
+                Console.WriteLine("Тесты не пройдены!");
+                return;
+            }
+
+            Console.WriteLine("Тесты пройдены!");
+
             int number = Task.GetNumber();
 
             Console.WriteLine($"{number}! = {Task.Factorial(number)}");
@@ -35,6 +45,34 @@ namespace Homework
                 return 1;
             }
             return n * Factorial(n - 1);
+        }
+    }
+
+    class Test
+    {
+        public static bool TestFactorial()
+        {
+            if (Task.Factorial(0) != 1)
+            {
+                return false;
+            }
+            if (Task.Factorial(9) != 362880)
+            {
+                return false;
+            }
+            if (Task.Factorial(5) != 120)
+            {
+                return false;
+            }
+            if (Task.Factorial(15) != 1307674368000)
+            {
+                return false;
+            }
+            if (Task.Factorial(12) != 479001600)
+            {
+                return false;
+            }
+            return true;
         }
     }
 }
