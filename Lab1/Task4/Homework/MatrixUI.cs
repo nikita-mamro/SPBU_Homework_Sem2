fdae0ac0daaa629054bcb1aaceb6b6ec4e053646
Nikita Mamro <43335327+zzzloba@users.noreply.github.com>
2019-02-16 19:04:35 +0300

    Matrix spiral traversal in C#

---
+++ Lab1/Task4/Homework/MatrixUI.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework
+{
+    class MatrixUI
+    {
+        private static int GetSize()
+        {
+            Console.Write("Введите нечётное N - размер матрицы NxN: ");
+            var inputString = Console.ReadLine();
+
+            if (!int.TryParse(inputString, out int N) || N < 0 || N % 2 == 0)
+            {
+                Console.WriteLine("N должно быть нечётным неотрицательным числом.");
+                return GetSize();
+            }
+
+            return N;
+        }
+
+        public static int[,] InitMatrix()
+        {
+            var N = GetSize();
+
+            var res = new int[N,N];
+
+            for (int i = 0; i < N; ++i)
+            {
+                Console.WriteLine($"Введите поочерёдно элементы {i + 1}-й строки.");
+
+                for (int j = 0; j < N; ++j)
+                {
+                    var inputElement = Console.ReadLine();
+
+                    if (!int.TryParse(inputElement, out int element))
+                    {
+                        Console.WriteLine("Элемент матрицы должен быть числом.");
+                        --j;
+                        continue;
+                    }
+
+                    res[i, j] = element;
+                }
+            }
+
+            return res;
+        }
+
+        public static void PrintMatrixSquare(int[,] theMatrix)
+        {
+            for (int i = 0; i < theMatrix.GetLength(0); ++i)
+            {
+                for (int j = 0; j < theMatrix.GetLength(1); ++j)
+                {
+                    Console.Write(String.Format("{0,4}", theMatrix[i, j]));
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Lab1/Task4/Homework/Test.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework
+{
+    class Test
+    {
+        public static bool TestTraversal()
+        {
+            var listCWFor3x3 = new List<int>();
+            var listCCWFor3x3 = new List<int>();
+
+            var matrix3x3 = new int[3,3];
+
+            int number = 1;
+
+            for (int i = 0; i < 3; ++i)
+            {
+                for (int j = 0; j < 3; ++j)
+                {
+                    matrix3x3[i, j] = number;
+                    ++number;
+                }
+            }
+
+            listCWFor3x3.Add(5);
+            listCWFor3x3.Add(6);
+            listCWFor3x3.Add(9);
+            listCWFor3x3.Add(8);
+            listCWFor3x3.Add(7);
+            listCWFor3x3.Add(4);
+            listCWFor3x3.Add(1);
+            listCWFor3x3.Add(2);
+            listCWFor3x3.Add(3);
+
+            listCCWFor3x3.Add(5);
+            listCCWFor3x3.Add(4);
+            listCCWFor3x3.Add(7);
+            listCCWFor3x3.Add(8);
+            listCCWFor3x3.Add(9);
+            listCCWFor3x3.Add(6);
+            listCCWFor3x3.Add(3);
+            listCCWFor3x3.Add(2);
+            listCCWFor3x3.Add(1);
+
+            var resCW3x3 = Task.GetMatrixSpiralTraversal(matrix3x3, 1);
+            var resCCW3x3 = Task.GetMatrixSpiralTraversal(matrix3x3, -1);
+
+            if (listCWFor3x3.Count != resCW3x3.Count || listCCWFor3x3.Count != resCCW3x3.Count)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < listCWFor3x3.Count; ++i)
+            {
+                if (listCWFor3x3[i] != listCWFor3x3[i])
+                {
+                    return false;
+                }
+            }
+
+            for (int i = 0; i < listCCWFor3x3.Count; ++i)
+            {
+                if (listCCWFor3x3[i] != resCCW3x3[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
