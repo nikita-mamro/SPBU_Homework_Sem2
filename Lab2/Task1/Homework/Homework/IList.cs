b36f37ccd5e7626ef6e97a0c920aeb78387c9d09
Nikita Mamro <43335327+zzzloba@users.noreply.github.com>
2019-02-22 22:46:02 +0300

    List in c# in correct branch

---
+++ Lab2/Task1/Homework/Homework/IList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework
+{
+    public interface IList
+    {
+        bool Add(int data, int position);
+        bool Remove(int position);
+        int Size();
+        bool isEmpty();
+        int GetDataByPosition(int position);
+        bool SetDataByPosition(int data, int position);
+        void Print();
+    }
+}
