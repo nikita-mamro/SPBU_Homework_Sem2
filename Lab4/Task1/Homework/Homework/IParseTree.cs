0e3f318bf4e31e1dae75748c459ae23c86efc234
Nikita Mamro <43335327+zzzloba@users.noreply.github.com>
2019-03-15 22:46:45 +0300

    Creating the structure of the future program

---
+++ Lab4/Task1/Homework/Homework/IParseTree.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace Homework
+{
+    /// <summary>
+    /// Интерфейс дерева разбора арифметического выражения
+    /// </summary>
+    public interface IParseTree
+    {
+        /// <summary>
+        /// Считает значение полученного из файла "input.txt" арифметического выражения
+        /// </summary>
+        /// <returns>Значение выражения</returns>
+        int Calculate();
+    }
+}
---
+++ Lab4/Task1/Homework/Homework/ParseTree.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+
+namespace Homework
+{
+    /// <summary>
+    /// Класс, реализующий дерево арифметического разбора
+    /// </summary>
+    public class ParseTree : IParseTree
+    {
+        /// <summary>
+        /// Класс, реализующий узел дерева
+        /// </summary>
+        private abstract class Node
+        {
+            /// <summary>
+            /// Возвращает значение арифметического выражения, представимого в виде дерева разбора с корнем в данном узле
+            /// </summary>
+            /// <returns></returns>
+            abstract public int Calculate();
+        }
+
+        private class Operand : Node
+        {
+            private int Data;
+
+            public override int Calculate()
+            {
+                
+            }
+
+            /// <summary>
+            /// Конструктор класса Operand
+            /// </summary>
+            /// <param name="data"></param>
+            public Operand(string data)
+            {
+                if (!int.TryParse(data, out int addableData))
+                {
+                    throw new ArgumentException("Выражение задано в неверном формате :( \n Проверьте правильность введённых данных.");
+                }
+
+                Data = addableData;
+            }
+        }
+
+        /// <summary>
+        /// Класс, реализующий оператор как элемент дерева разбора
+        /// </summary>
+        private class Operator : Node
+        {
+            private char data;
+            private Node left;
+            private Node right;
+
+            public override int Calculate()
+            {
+
+            }
+
+            public Operator(string data)
+            {
+
+            }
+        }
+
+        /// <summary>
+        /// Узел-корень дерева
+        /// </summary>
+        private Node root;
+
+        public int Calculate()
+        {
+            return 0;
+        }
+    }
+}
