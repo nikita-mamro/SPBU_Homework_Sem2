bcc93896c709c293d11bfdae9c28475c81591ee6
Nikita Mamro <43335327+zzzloba@users.noreply.github.com>
2019-03-30 01:31:18 +0300

    Have implemented  6.1 methods

---
+++ Lab6/Task1/Homework/Homework/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<char> list = Functions.Map(new List<int>() { 100, 201, 230 }, x => (char)x);
+
+            foreach (var element in list)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
