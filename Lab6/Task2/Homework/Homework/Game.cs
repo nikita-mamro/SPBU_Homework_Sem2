705800999215a9aebea335fd45e03c90e071664f
Nikita Mamro <43335327+zzzloba@users.noreply.github.com>
2019-03-31 13:11:19 +0300

    Initial 6.2 stuff

---
+++ Lab6/Task2/Homework/Homework/Game.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Game
+    {
+        public void OnLeft(object sender, EventArgs args)
+        {
+            Console.WriteLine("Going left");
+        }
+
+        public void OnRight(object sender, EventArgs args)
+        {
+            Console.WriteLine("Going right");
+        }
+
+        public void OnUp(object sender, EventArgs args)
+        {
+            Console.WriteLine("Going up");
+        }
+
+        public void OnDown(object sender, EventArgs args)
+        {
+            Console.WriteLine("Going down");
+        }
+    }
+}
---
+++ Lab6/Task2/Homework/Homework/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var eventLoop = new EventLoop();
+            var game = new Game();
+
+            eventLoop.LeftHandler += game.OnLeft;
+            eventLoop.RightHandler += game.OnRight;
+            eventLoop.UpHandler += game.OnUp;
+            eventLoop.DownHandler += game.OnDown;
+
+            eventLoop.Run();
+        }
+    }
+}
