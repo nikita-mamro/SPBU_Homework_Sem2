97fa9fcdecc466119815dc40f62d31c8e901ab49
Nikita Mamro <43335327+zzzloba@users.noreply.github.com>
2019-04-10 00:49:37 +0300

    Less copypaste

--- Lab3/Task2/Homework/HomeworkTests/HashTableTests.cs
+++ Lab3/Task2/Homework/HomeworkTests/HashTableTests.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Homework;
 using System;
+using System.Collections.Generic;
 
 namespace Homework.Tests
 {
@@ -10,337 +11,142 @@ namespace Homework.Tests
     [TestClass()]
     public class HashTableTests
     {
-        private IHashTable tableAdler;
-        private IHashTable tableJenkins;
-        private IHashTable tableMurmur2;
-
-        [TestInitialize()]
-        public void Initialize()
-        {
-            tableAdler = new HashTable(new AdlerHash());
-            tableJenkins = new HashTable(new JenkinsHash());
-            tableMurmur2 = new HashTable(new Murmur2Hash());
-        }
-
         /// <summary>
         /// Тесты добавления
         /// </summary>
-        [TestMethod()]
-        public void AddToEmptyAdlerTest()
+        [DynamicData("TestMethodInput")]
+        [TestMethod]
+        public void AddToEmptyTest(IHashTable table)
         {
-            tableAdler.Add("hello");
-            Assert.AreEqual(1, tableAdler.Count);
+            table.Add("hello");
+            Assert.AreEqual(1, table.Count);
         }
 
-        [TestMethod()]
-        public void AddToEmptyJenkinsTest()
+        [DynamicData("TestMethodInput")]
+        [TestMethod]
+        public void CountUpdatesWhenElementAddedTest(IHashTable table)
         {
-            tableJenkins.Add("hello");
-            Assert.AreEqual(1, tableJenkins.Count);
+            table.Add("hello");
+            var oldCount = table.Count;
+            table.Add("bye");
+
+            Assert.AreEqual(oldCount + 1, table.Count);
         }
 
-        [TestMethod()]
-        public void AddToEmptyMurmur2Test()
-        {
-            tableMurmur2.Add("hello");
-            Assert.AreEqual(1, tableMurmur2.Count);
-        }
-
-        [TestMethod()]
-        public void CountUpdatesWhenElementAddedAdlerTest()
-        {
-            tableAdler.Add("hello");
-            var oldCount = tableAdler.Count;
-            tableAdler.Add("bye");
-
-            Assert.AreEqual(oldCount + 1, tableAdler.Count);
-        }
-
-        [TestMethod()]
-        public void CountUpdatesWhenElementAddedJenkinsTest()
-        {
-            tableJenkins.Add("hello");
-            var oldCount = tableJenkins.Count;
-            tableJenkins.Add("bye");
-
-            Assert.AreEqual(oldCount + 1, tableJenkins.Count);
-        }
-
-        [TestMethod()]
-        public void CountUpdatesWhenElementAddedMurmur2Test()
-        {
-            tableMurmur2.Add("hello");
-            var oldCount = tableMurmur2.Count;
-            tableMurmur2.Add("bye");
-
-            Assert.AreEqual(oldCount + 1, tableMurmur2.Count);
-        }
-
-        [TestMethod()]
-        public void AddManyElementsAdlerTest()
+        [DynamicData("TestMethodInput")]
+        [TestMethod]
+        public void AddManyElementsTest(IHashTable table)
         {
             for (var i = 0; i < 50; ++i)
             {
-                tableAdler.Add(i.ToString());
+                table.Add(i.ToString());
             }
 
-            Assert.AreEqual(50, tableAdler.Count);
+            Assert.AreEqual(50, table.Count);
         }
 
-        [TestMethod()]
-        public void AddManyElementsJenkinsTest()
+        [DynamicData("TestMethodInput")]
+        [TestMethod]
+        public void CountUpdatesWhenAddingExistingTest(IHashTable table)
         {
             for (var i = 0; i < 50; ++i)
             {
-                tableJenkins.Add(i.ToString());
+                table.Add(i.ToString());
+                table.Add(i.ToString());
             }
 
-            Assert.AreEqual(50, tableJenkins.Count);
-        }
-
-        [TestMethod()]
-        public void AddManyElementsMurmur2Test()
-        {
-            for (var i = 0; i < 50; ++i)
-            {
-                tableMurmur2.Add(i.ToString());
-            }
-
-            Assert.AreEqual(50, tableMurmur2.Count);
-        }
-
-        [TestMethod()]
-        public void CountUpdatesWhenAddingExistingAdlerTest()
-        {
-            for (var i = 0; i < 50; ++i)
-            {
-                tableAdler.Add(i.ToString());
-                tableAdler.Add(i.ToString());
-            }
-
-            Assert.AreEqual(100, tableAdler.Count);
-        }
-
-        [TestMethod()]
-        public void CountUpdatesWhenAddingExistingJenkinsTest()
-        {
-            for (var i = 0; i < 50; ++i)
-            {
-                tableJenkins.Add(i.ToString());
-                tableJenkins.Add(i.ToString());
-            }
-
-            Assert.AreEqual(100, tableJenkins.Count);
-        }
-
-        [TestMethod()]
-        public void CountUpdatesWhenAddingExistingMurmur2Test()
-        {
-            for (var i = 0; i < 50; ++i)
-            {
-                tableMurmur2.Add(i.ToString());
-                tableMurmur2.Add(i.ToString());
-            }
-
-            Assert.AreEqual(100, tableMurmur2.Count);
+            Assert.AreEqual(100, table.Count);
         }
 
         /// <summary>
-        /// Тесты IsContained
+        /// Тест IsContained
         /// </summary>
-        [TestMethod()]
-        public void IsContainedAdlerTest()
+        [DynamicData("TestMethodInput")]
+        [TestMethod]
+        public void IsContainedTest(IHashTable table)
         {
-            tableAdler.Add("hello");
-            Assert.IsTrue(tableAdler.IsContained("hello") && !tableAdler.IsContained("HELLO"));
-        }
-
-        [TestMethod()]
-        public void IsContainedJenkinsTest()
-        {
-            tableJenkins.Add("hello");
-            Assert.IsTrue(tableJenkins.IsContained("hello") && !tableJenkins.IsContained("HELLO"));
-        }
-
-        [TestMethod()]
-        public void IsContainedMurmur2Test()
-        {
-            tableMurmur2.Add("hello");
-            Assert.IsTrue(tableMurmur2.IsContained("hello") && !tableMurmur2.IsContained("HELLO"));
+            table.Add("hello");
+            Assert.IsTrue(table.IsContained("hello") && !table.IsContained("HELLO"));
         }
 
         /// <summary>
         /// Тест удаления
         /// </summary>
-        [TestMethod()]
+        [DynamicData("TestMethodInput")]
+        [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void RemoveFromEmptyExceptionAdlerTest()
+        public void RemoveFromEmptyExceptionTest(IHashTable table)
         {
-            tableAdler.Remove("hello");
+            table.Remove("hello");
         }
 
-        [TestMethod()]
+        [DynamicData("TestMethodInput")]
+        [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void RemoveFromEmptyExceptionJenkinsTest()
+        public void RemoveElementWhichDoesNotExistExceptionTest(IHashTable table)
         {
-            tableJenkins.Remove("hello");
+            table.Add("hello");
+            table.Remove("haha");
         }
 
-        [TestMethod()]
-        [ExpectedException(typeof(ArgumentException))]
-        public void RemoveFromEmptyExceptionMurmur2Test()
-        {
-            tableMurmur2.Remove("hello");
-        }
-
-        [TestMethod()]
-        [ExpectedException(typeof(ArgumentException))]
-        public void RemoveElementWhichDoesNotExistExceptionAdlerTest()
-        {
-            tableAdler.Add("hello");
-            tableAdler.Remove("haha");
-        }
-
-        [TestMethod()]
-        [ExpectedException(typeof(ArgumentException))]
-        public void RemoveElementWhichDoesNotExistExceptionJenkinsTest()
-        {
-            tableJenkins.Add("hello");
-            tableJenkins.Remove("haha");
-        }
-
-        [TestMethod()]
-        [ExpectedException(typeof(ArgumentException))]
-        public void RemoveElementWhichDoesNotExistExceptionMurmur2Test()
-        {
-            tableMurmur2.Add("hello");
-            tableMurmur2.Remove("haha");
-        }
-
-        [TestMethod()]
-        public void CountUpdateRemoveFromTableOfManyDifferentElementsAdlerTest()
+        [DynamicData("TestMethodInput")]
+        [TestMethod]
+        public void CountUpdateRemoveFromTableOfManyDifferentElementsTest(IHashTable table)
         {
             for (var i = 0; i < 50; ++i)
             {
-                tableAdler.Add(i.ToString());
+                table.Add(i.ToString());
             }
 
-            var oldCount = tableAdler.Count;
-            tableAdler.Remove("10");
+            var oldCount = table.Count;
+            table.Remove("10");
 
-            Assert.AreEqual(oldCount - 1, tableAdler.Count);
+            Assert.AreEqual(oldCount - 1, table.Count);
         }
 
-        [TestMethod()]
-        public void CountUpdateRemoveFromTableOfManyDifferentElementsJenkinsTest()
+        [DynamicData("TestMethodInput")]
+        [TestMethod]
+        public void CountUpdateRemoveFromTableOfManySameElementsTest(IHashTable table)
         {
             for (var i = 0; i < 50; ++i)
             {
-                tableJenkins.Add(i.ToString());
+                table.Add("same thing");
             }
 
-            var oldCount = tableJenkins.Count;
-            tableJenkins.Remove("10");
+            var oldCount = table.Count;
+            table.Remove("same thing");
 
-            Assert.AreEqual(oldCount - 1, tableJenkins.Count);
-        }
-
-        [TestMethod()]
-        public void CountUpdateRemoveFromTableOfManyDifferentElementsMurmur2Test()
-        {
-            for (var i = 0; i < 50; ++i)
-            {
-                tableMurmur2.Add(i.ToString());
-            }
-
-            var oldCount = tableMurmur2.Count;
-            tableMurmur2.Remove("10");
-
-            Assert.AreEqual(oldCount - 1, tableMurmur2.Count);
-        }
-
-        [TestMethod()]
-        public void CountUpdateRemoveFromTableOfManySameElementsAdlerTest()
-        {
-            for (var i = 0; i < 50; ++i)
-            {
-                tableAdler.Add("same thing");
-            }
-
-            var oldCount = tableAdler.Count;
-            tableAdler.Remove("same thing");
-
-            Assert.AreEqual(oldCount - 1, tableAdler.Count);
-        }
-
-        [TestMethod()]
-        public void CountUpdateRemoveFromTableOfManySameElementsJenkinsTest()
-        {
-            for (var i = 0; i < 50; ++i)
-            {
-                tableJenkins.Add("same thing");
-            }
-
-            var oldCount = tableJenkins.Count;
-            tableJenkins.Remove("same thing");
-
-            Assert.AreEqual(oldCount - 1, tableJenkins.Count);
-        }
-
-        [TestMethod()]
-        public void CountUpdateRemoveFromTableOfManySameElementsMurmur2Test()
-        {
-            for (var i = 0; i < 50; ++i)
-            {
-                tableMurmur2.Add("same thing");
-            }
-
-            var oldCount = tableMurmur2.Count;
-            tableMurmur2.Remove("same thing");
-
-            Assert.AreEqual(oldCount - 1, tableMurmur2.Count);
+            Assert.AreEqual(oldCount - 1, table.Count);
         }
 
         /// <summary>
         /// Тест метода Clear()
         /// </summary>
-        [TestMethod()]
-        public void ClearAdlerTest()
+        [DynamicData("TestMethodInput")]
+        [TestMethod]
+        public void ClearTest(IHashTable table)
         {
             for (var i = 0; i < 50; ++i)
             {
-                tableAdler.Add(i.ToString());
+                table.Add(i.ToString());
             }
 
-            tableAdler.Clear();
+            table.Clear();
 
-            Assert.AreEqual(0, tableAdler.Count);
+            Assert.AreEqual(0, table.Count);
         }
 
-        [TestMethod()]
-        public void ClearJenkinsTest()
+        public static IEnumerable<object[]> TestMethodInput
         {
-            for (var i = 0; i < 50; ++i)
+            get
             {
-                tableJenkins.Add(i.ToString());
+                return new[]
+                {
+                    new object[] { new HashTable(new AdlerHash()) },
+                    new object[] { new HashTable(new Murmur2Hash()) },
+                    new object[] { new HashTable(new JenkinsHash())}
+                };
             }
-
-            tableJenkins.Clear();
-
-            Assert.AreEqual(0, tableJenkins.Count);
-        }
-
-        [TestMethod()]
-        public void ClearMurmur2Test()
-        {
-            for (var i = 0; i < 50; ++i)
-            {
-                tableMurmur2.Add(i.ToString());
-            }
-
-            tableMurmur2.Clear();
-
-            Assert.AreEqual(0, tableMurmur2.Count);
         }
     }
 }
