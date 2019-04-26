2a05d8872bf5b26c672ac606c70daff60d473313
Nikita Mamro <43335327+zzzloba@users.noreply.github.com>
2019-04-26 14:05:44 +0300

    Trolling button

---
+++ Test1Try2/TestTask/TestTask/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace TestTask
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RunningButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // RunningButton
+            // 
+            this.RunningButton.BackColor = System.Drawing.Color.Red;
+            this.RunningButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.RunningButton.ForeColor = System.Drawing.Color.White;
+            this.RunningButton.Location = new System.Drawing.Point(0, 1);
+            this.RunningButton.Name = "RunningButton";
+            this.RunningButton.Size = new System.Drawing.Size(400, 553);
+            this.RunningButton.TabIndex = 0;
+            this.RunningButton.Text = "Press Me";
+            this.RunningButton.UseVisualStyleBackColor = false;
+            this.RunningButton.Click += new System.EventHandler(this.RunningButton_Click);
+            this.RunningButton.MouseEnter += new System.EventHandler(this.RunningButton_MouseEnter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 553);
+            this.Controls.Add(this.RunningButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button RunningButton;
+    }
+}
+
---
+++ Test1Try2/TestTask/TestTask/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestTask
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Обработчик нажатия на кнопку RunningButton
+        /// </summary>
+        private void RunningButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        /// <summary>
+        /// Обработчик события "Пользователь пытается навести курсор на кнопку RunningButton"
+        /// </summary>
+        private void RunningButton_MouseEnter(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            RunningButton.Location = new Point((RunningButton.Location.X + Width / 2) % Width, RunningButton.Location.Y);
+            RunningButton.BackColor = Color.FromArgb(r.Next(0, 256), r.Next(0, 256), r.Next(0, 256));
+            RunningButton.ForeColor = Color.FromArgb(r.Next(0, 256), r.Next(0, 256), r.Next(0, 256));
+        }
+    }
+}
