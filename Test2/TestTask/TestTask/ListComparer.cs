7df46b08408d9bc3300ac82f0799c7d0fa8a8f5c
Nikita Mamro <43335327+zzzloba@users.noreply.github.com>
2019-05-10 15:00:34 +0300

    Some code, w/out tests yet

---
+++ Test2/TestTask/TestTask/ListComparer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestTask
+{
+    public class ListComparer<T> : IComparer<List<T>>
+    {
+        public int Compare(List<T> x, List<T> y)
+        {
+            return x.Count.CompareTo(y.Count);
+        }
+    }
+}
---
+++ Test2/TestTask/TestTask/StringSeparator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestTask
+{
+    static public class StringSeparator
+    {
+        public static List<string> GetListOfWords(string sentence)
+        {
+            var word = "";
+            var listOfWords = new List<string>();
+
+            foreach (var symbol in sentence)
+            {
+                if (symbol == ' ')
+                {
+                    if (word.Length > 0)
+                    {
+                        listOfWords.Add(word);
+                    }
+
+                    word = "";
+                    continue;
+                }
+
+                word += symbol;
+            }
+
+            if (word.Length > 0)
+            {
+                listOfWords.Add(word);
+            }
+
+            return listOfWords;
+        }
+    }
+}
