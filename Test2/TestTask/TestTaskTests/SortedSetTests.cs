f32747e5ea7cb44851068f602b2b8faf5bc65ac9
Nikita Mamro <43335327+zzzloba@users.noreply.github.com>
2019-05-10 15:16:11 +0300

    Some tests, started set tests

---
+++ Test2/TestTask/TestTaskTests/SortedSetTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestTask;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask.Tests
+{
+    [TestClass]
+    public class SortedSetTests
+    {
+        [TestMethod]
+        public void AddTest()
+        {
+            
+        }
+
+        [TestMethod]
+        public void GetSortedElementsTest()
+        {
+            
+        }
+    }
+}
