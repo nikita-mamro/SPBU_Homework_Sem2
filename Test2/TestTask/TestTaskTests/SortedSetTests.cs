6fc2238d2346ece8bd93941d4612d650f632ea0b
Nikita Mamro <43335327+zzzloba@users.noreply.github.com>
2019-05-10 15:19:56 +0300

    Small set test, at least sth

--- Test2/TestTask/TestTaskTests/SortedSetTests.cs
+++ Test2/TestTask/TestTaskTests/SortedSetTests.cs
@@ -12,15 +12,38 @@ namespace TestTask.Tests
     public class SortedSetTests
     {
         [TestMethod]
-        public void AddTest()
-        {
-            
-        }
-
-        [TestMethod]
         public void GetSortedElementsTest()
         {
-            
+            var strings = new List<string>();
+
+            strings.Add("One two three four five");
+            strings.Add("One two three four");
+            strings.Add("One two three");
+            strings.Add("One two");
+            strings.Add("One");
+
+            var lists = new List<List<string>>();
+
+            foreach (var str in strings)
+            {
+                lists.Add(StringSeparator.GetListOfWords(str));
+            }
+
+            var set = new SortedSet<string>(lists);
+
+            var sortedStrings = set.GetSortedElements();
+
+            var currentCount = -1;
+
+            foreach (var sentence in sortedStrings)
+            {
+                if (sentence.Count < currentCount)
+                {
+                    Assert.Fail();
+                }
+
+                currentCount = sentence.Count;
+            }
         }
     }
 }
