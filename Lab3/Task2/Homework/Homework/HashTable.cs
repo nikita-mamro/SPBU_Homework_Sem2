c2b2a8d443dfa1e45fddb177a71ae18a5b24f5fe
Nikita Mamro <43335327+zzzloba@users.noreply.github.com>
2019-05-31 12:57:17 +0300

    Updated branch from master

---
+++ Lab3/Task2/Homework/Homework/HashTable.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework
+{
+    /// <summary>
+    /// Класс, реализующий хэш-таблицу
+    /// </summary>
+    public class HashTable : IHashTable
+    {
+        /// <summary>
+        /// Слоты таблицы, то есть списки, в которых хранятся элементы таблицы
+        /// </summary>
+        private List<IList> buckets;
+
+        public int Count { get; private set; }
+
+        /// <summary>
+        /// Количество неповторяющихся значений
+        /// </summary>
+        private int differentCount;
+
+        /// <summary>
+        /// Отвечает за выбор реализации хэш функции
+        /// </summary>
+        private IHashFunction hashImplementation;
+
+        /// <summary>
+        /// Конструктор хэш-таблицы с выбранной реализацией хэш-функции
+        /// </summary>
+        /// <param name="hashImplementation">Реализация хэш-функции</param>
+        public HashTable(IHashFunction hashImplementation)
+        {
+            this.hashImplementation = hashImplementation;
+
+            buckets = new List<IList>();
+
+            const int DefaultSize = 10;
+
+            for (var i = 0; i < DefaultSize; ++i)
+            {
+                buckets.Add(new List());
+            }
+        }
+
+        /// <summary>
+        /// Увеличение количества слотов хэш-таблицы
+        /// </summary>
+        private void Expand()
+        {
+            var newBuckets = new List<IList>();
+
+            for (var i = 0; i < buckets.Count * 2; ++i)
+            {
+                newBuckets.Add(new List());
+            }
+
+            foreach (var list in buckets)
+            {
+                if (list.IsEmpty)
+                {
+                    continue;
+                }
+
+                var words = list.GetWords();
+
+                foreach (var word in words)
+                {
+                    var hash = Math.Abs((int)(hashImplementation.HashFunction(word) % newBuckets.Count));
+                    newBuckets[hash].Add(word);
+                }
+            }
+
+            buckets = newBuckets;
+        }
+
+        public void Add(string word)
+        {
+            if (LoadCoefficient > 1)
+            {
+                Expand();
+            }
+
+            var hash = Math.Abs((int)(hashImplementation.HashFunction(word) % buckets.Count));
+
+            if (buckets[hash].IsEmpty)
+            {
+                buckets[hash] = new List();
+                ++differentCount;
+            }
+            else if (!buckets[hash].Contains(word))
+            {
+                ++differentCount;
+            }
+
+            buckets[hash].Add(word);
+            ++Count;
+        }
+
+        public void Remove(string word)
+        {
+            if (!IsContained(word))
+            {
+                throw new ArgumentException("Слово не найдено в наборе!");
+            }
+
+            var hash = Math.Abs((int)(hashImplementation.HashFunction(word) % buckets.Count));
+
+            if (!buckets[hash].Remove(word))
+            {
+                return;
+            }
+
+            --Count;
+
+            if (!buckets[hash].Contains(word))
+            {
+                --differentCount;
+            }
+        }
+
+        public bool IsContained(string word)
+        {
+            var hash = Math.Abs((int)(hashImplementation.HashFunction(word) % buckets.Count));
+            return buckets[hash].Contains(word);
+        }
+
+        /// <summary>
+        /// Коэффициент заполненности хэш-таблицы 
+        /// (отношение количества отличных друг от друга элементов к количеству слотов)
+        /// </summary>
+        private double LoadCoefficient
+            => (double)differentCount / (double)buckets.Count;
+
+        public void Clear()
+        {
+            foreach (var list in buckets)
+            {
+                list.Clear();
+            }
+
+            Count = 0;
+            differentCount = 0;
+        }
+    }
+}
---
+++ Test1Try2/TestTask/TestTask/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace TestTask
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.TrollingButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TrollingButton
+            // 
+            this.TrollingButton.BackColor = System.Drawing.Color.Red;
+            this.TrollingButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.TrollingButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TrollingButton.ForeColor = System.Drawing.Color.White;
+            this.TrollingButton.Location = new System.Drawing.Point(0, 0);
+            this.TrollingButton.Name = "TrollingButton";
+            this.TrollingButton.Size = new System.Drawing.Size(400, 555);
+            this.TrollingButton.TabIndex = 0;
+            this.TrollingButton.Text = "Press Me!";
+            this.TrollingButton.UseVisualStyleBackColor = false;
+            this.TrollingButton.Click += new System.EventHandler(this.RunningButton_Click);
+            this.TrollingButton.MouseEnter += new System.EventHandler(this.RunningButton_MouseEnter);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 553);
+            this.Controls.Add(this.TrollingButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button TrollingButton;
+    }
+}
+
